b31fb918c108b203c00e5aedc8c0a8587be1750d
Jack Davison-Gates <36479356+jack-york@users.noreply.github.com>
2020-05-18 18:32:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,99 +1,145 @@
-var gameData = {
-  currentPower: 0,
-  totalPower: 0,
-  powerPerClick: 1,
-  powerPerTick: 1,
-  capasitors: 1,
-  capasitorsStorage: 100,
-  capasitorCost: 20,
-  batteries: 0,
-  batteriesStorage: 3000,
-  batteriesEfficency: 0.3,
-  batteryCost: 500,
-  batteryStorage: 3000,
-  powerPerTickCost: 10,
-  totalPowerStorage: 100,
-  turbineSpeed: 0,
-  turbineMinSpeed : 0,
-  turbineMass: 3,
-  turbineMaxSpeed: 500000,
-  turbineFriction: 0.005,
-  generatorFriction: 0.0005,
-  generatorEfficency: 1,
-  turbineSpinForce: 10,
-  numberFormat: 2,
-  wood: 0,
-  coal: 0,
-  iron: 0,
-  steel: 0,
-  sand: 0,
-  glass: 0,
-  plastic: 0,
-  paper: 0,
-  oil: 0,
-  stone: 0,
-  diggers: 0,
-  diggerCost: 50,
-  diggerEfficiency: 0.01,
-  mines: 0,
-  mineCost: 100,
-  mineEfficiency: 0.01,
-  drils: 0,
-  drilCost: 100,
-  drilEfficiency: 0.01,
-  pumps: 0,
-  pumpCost: 100,
-  pumpEfficiency: 0.01
+var GameData = {
+    var PowerData = {
+        currentPower: 0,
+        totalPower: 0,
+        powerPerClick: 1,
+        powerPerTick: 1,
+    }
+    var PowerStorageData = {
+        capasitors: 1,
+        capasitorsStorage: 100,
+        capasitorCost: 20,
+        batteries: 0,
+        batteriesStorage: 3000,
+        batteriesEfficency: 0.3,
+        batteryCost: 500,
+        batteryStorage: 3000,
+        powerPerTickCost: 10,
+        totalPowerStorage: 100,
+    }
+    var TurbineData = {
+        turbineSpeed: 0,
+        turbineMinSpeed : 0,
+        turbineMass: 3,
+        turbineMaxSpeed: 500000,
+        turbineFriction: 0.005,
+        generatorFriction: 0.0005,
+        generatorEfficency: 1,
+        turbineSpinForce: 10,
+    }
+    var MiscellaneousData = {
+        numberFormat: 2,
+    }
 }
 
-var spaceData = {
-  telescopeLevel: 0,
-  telescopeCost: 100,
-  telescopeOn: "Off",
-  energyCost: 0,
-  area: 1,
-  spaceSerchSpeed: 0,
-  freeSpaceArea1: 1000,
-  freeSpaceArea2: 10000,
-  freeSpaceArea3: 100000,
-  area1Clear: 0,
-  area2Clear: 0,
-  area3Clear: 0,
-  planetAlpha: Math.random()*1000,
-  planetBata: Math.random()*1000,
-  planetGamma: Math.random()*1000,
-  planetDelta: Math.random()*10000,
-  planetEpsilon: Math.random()*10000,
-  planetZeta: Math.random()*10000,
-  planetEta: Math.random()*100000,
-  planetTheata: Math.random()*100000,
-  planetIota: Math.random()*100000
+var MaterialData = {
+    var StockpillData = {
+        wood: 0,
+        coal: 0,
+        iron: 0,
+        steel: 0,
+        sand: 0,
+        glass: 0,
+        plastic: 0,
+        paper: 0,
+        oil: 0,
+        stone: 0,
+    }
+    var BuildingData = {
+        diggers: 0,
+        mines: 0,
+        drils: 0,
+        pumps: 0,
+    }
+    var BuildingCostData = {
+        diggerCost: 50,
+        mineCost: 100,
+        drilCost: 100,
+        pumpCost: 100,
+    }
+    var BuildingEfficiencyData = {
+        diggerEfficiency: 0.01,
+        mineEfficiency: 0.01,
+        drilEfficiency: 0.01,
+        pumpEfficiency: 0.01,
+    }
 }
 
-var workerData = {
-    workers: 0,
-    freeWorkers: 0,
-    workerCost: 10000,
-    energyWorker: 0,
-    energyWorkerEfficiency: 5,
-    woodWorker: 0,
-    woodWorkerEfficiency: 5,
-    sandWorker: 0,
-    sandWorkerEfficiency: 5,
-    glassWorker: 0,
-    glassWorkerEfficiency: 0.5,
-    ironWorker: 0,
-    ironWorkerEfficiency: 0.01,
-    coalWorker: 0,
-    coalWorkerEfficiency: 0.01,
-    steelWorker: 0,
-    steelWorkerEfficiency: 0.001,
-    oilWorker: 0,
-    oilWorkerEfficiency: 0.001,
-    plasticWorker: 0,
-    plasticWorkerEfficiency: 0.0001
+var SpaceData = {
+    var TelescopeData = {
+        telescopeLevel: 0,
+        telescopeCost: 100,
+        telescopeStatus: "Off",
+        telescopeEnergyCost: 0,
+        serchArea: 1,
+        areaSerchSpeed: 0,
+    }
+    var SerchAreaData = {
+        freeSpaceArea1: 1000,
+        freeSpaceArea2: 10000,
+        freeSpaceArea3: 100000,
+        area1Clear: 0,
+        area2Clear: 0,
+        area3Clear: 0,
+    }
+    var AlphaData = {
+        alphaLocation: Math.random()*1000,
+    }
+    var BataData = {
+        bataLocation: Math.random()*1000,
+    }
+    var GammaData = {
+        gammaLocation: Math.random()*1000,
+    }
+    var DeltaData = {
+        deltaLocation: Math.random()*10000,
+    }
+    var EpsilonData = {
+        epsilonLocation: Math.random()*10000,
+    }
+    var ZetaData = {
+        zetaLocation: Math.random()*10000,
+    }
+    var EtaData = {
+        etaLocation: Math.random()*100000,
+    }
+    var TheataData = {
+        theataLocation: Math.random()*100000,
+    }
+    var IotaData = {
+        iotaLocation: Math.random()*100000,
+    }
 }
 
+var WorkerData = {
+    var WorkerStatusData = {
+        workers: 0,
+        freeWorkers: 0,
+        workerCost: 10000,
+    }
+    var JobData = {
+        energyWorker: 0,
+        woodWorker: 0,
+        sandWorker: 0,
+        glassWorker: 0,
+        ironWorker: 0,
+        coalWorker: 0,
+        steelWorker: 0,
+        oilWorker: 0,
+        plasticWorker: 0,
+    }
+    var JobEfficiencyData = {
+        energyWorkerEfficiency: 5,
+        woodWorkerEfficiency: 5,
+        sandWorkerEfficiency: 5,
+        glassWorkerEfficiency: 0.5,
+        ironWorkerEfficiency: 0.01,
+        coalWorkerEfficiency: 0.01,
+        steelWorkerEfficiency: 0.001,
+        oilWorkerEfficiency: 0.001,
+        plasticWorkerEfficiency: 0.0001,
+    }
+}
 function formatNumber(number) {
   if (number > 5000000000000000000) {
     number /= 1000000000000000000
