a3c810330feb3c8ee73899b2f07f0a4bb78728b0
LiamDavisonGates <49875459+LiamDavisonGates@users.noreply.github.com>
2020-05-20 22:01:17 +0100

    added a working chart

--- main.js
+++ main.js
@@ -35,6 +35,42 @@ var MiscellaneousData = {
     gameSpeed: 250,
 }
 
+var data = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  datasets: [{
+    label: "Dataset #1",
+    backgroundColor: "rgba(255,99,132,0.2)",
+    borderColor: "rgba(255,99,132,1)",
+    borderWidth: 2,
+    hoverBackgroundColor: "rgba(255,99,132,0.4)",
+    hoverBorderColor: "rgba(255,99,132,1)",
+    data: [65, 59, 20, 81, 56, 55, 40],
+  }]
+};
+
+var option = {
+  responsive: false,
+  scales: {
+    yAxes: [{
+      stacked: true,
+      gridLines: {
+        display: true,
+        color: "rgba(255,99,132,0.2)"
+      }
+    }],
+    xAxes: [{
+      gridLines: {
+        display: false
+      }
+    }]
+  }
+};
+
+Chart.Bar('myChart', {
+  options: option,
+  data: data
+});
+
 function formatNumber(number) {
     if (number > 5000000000000000000) {
         number /= 1000000000000000000
