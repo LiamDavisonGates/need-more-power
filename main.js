2daa0b581e97d7391cf7900962ebc945cffe789d
LiamDavisonGates <49875459+LiamDavisonGates@users.noreply.github.com>
2020-05-20 22:06:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -71,26 +71,6 @@ Chart.Bar('myChart', {
   data: data
 });
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
 function formatNumber(number) {
     if (number > 5000000000000000000) {
         number /= 1000000000000000000
